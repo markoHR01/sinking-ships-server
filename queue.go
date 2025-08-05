9054e40aee59def25ad73ce728d6bd711daf51ab
markoHR01 <marko.marko2adm@gmail.com>
2025-08-05 17:11:38 +0200

    Added Queue, Join, Leave

---
+++ queue.go
@@ -0,0 +1,42 @@
+package main
+
+import "sync"
+
+type Queue struct {
+	mu   sync.Mutex
+	waiting  []*Client
+}
+
+func NewQueue() *Queue {
+	return &Queue{
+		waiting: make([]*Client, 0),
+	}
+}
+
+func (q *Queue) Join(client *Client) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	q.waiting = append(q.waiting, client)
+
+	if len(q.waiting) >= 2 {
+		client1 := q.waiting[0]
+		client2 := q.waiting[1]
+		q.waiting = q.waiting[2:]
+
+		client1.SendMessage(Message{"type": "MatchFound"})
+		client2.SendMessage(Message{"type": "MatchFound"})
+	}
+}
+
+func (q *Queue) Leave(client *Client) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	for i, c := range q.waiting {
+		if c == client {
+			q.waiting = append(q.waiting[:i], q.waiting[i+1:]...)
+			break
+		}
+	}
+}
