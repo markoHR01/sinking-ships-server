cb3272a773d12f2218a814f651578ed209b249a7
markoHR01 <marko.marko2adm@gmail.com>
2025-08-05 10:27:53 +0200

    Reworked client and message processing

--- main.go
+++ main.go
@@ -3,48 +3,25 @@ package main
 import (
 	"fmt"
 	"net"
-	"strings"
 )
 
 const serverPort = ":22335"
 
-func readMessage(data []byte) map[string]string {
-	message := make(map[string]string)
-	input := string(data)
-
-	msgLines := strings.Split(input, "\n")
-	for _, msgLine := range msgLines {
-		keyValue := strings.SplitN(msgLine, "=", 2)
-		if len(keyValue) == 2 {
-			key := keyValue[0]
-			value := keyValue[1]
-			message[key] = value
-		}
-	}
-
-	return message
-}
-
-func processConnection(conn net.Conn) {
-	defer conn.Close()
-
-	var buffer [1024] byte
+func processClientMessages(client *Client) {
+	defer client.Close()
 
 	for {
-		size, err := conn.Read(buffer[:])
+		message, err := client.ReadMessage()
 		if err != nil {
-			fmt.Println("conn.Read(buffer[:]) failed:", err)
+			fmt.Println("client.ReadMessage() failed:", err)
 			return
 		}
 
-		message := readMessage(buffer[:size])
-
 		switch message["type"] {
-
 		case "JoinQueue":
-			conn.Write([]byte("type=QueueJoined\nSTOP\n"))
+			client.SendMessage(Message{"type": "QueueJoined"})
 		case "LeaveQueue":
-			conn.Write([]byte("type=QueueLeft\nSTOP\n"))
+			client.SendMessage(Message{"type": "QueueLeft"})
 		}
 	}
 }
@@ -62,6 +39,7 @@ func main() {
 			continue
 		}
 
-		go processConnection(conn)
+		client := NewClient(conn)
+		go processClientMessages(client)
 	}
 }
