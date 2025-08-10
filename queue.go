44bd5859420e69ab769bcdf3e30e5c23dfa5f4d5
markoHR01 <marko.marko2adm@gmail.com>
2025-08-10 09:52:38 +0200

    Removed startMatch() from Join()

--- queue.go
+++ queue.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"sync"
-	"time"
-)
+import "sync"
 
 type Queue struct {
 	mu   sync.Mutex
@@ -18,25 +15,9 @@ func NewQueue() *Queue {
 
 func (q *Queue) Join(client *Client) {
 	q.mu.Lock()
+	defer q.mu.Unlock()
+
 	q.waiting = append(q.waiting, client)
-
-	if len(q.waiting) >= 2 {
-		client1 := q.waiting[0]
-		client2 := q.waiting[1]
-		q.waiting = q.waiting[2:]
-		q.mu.Unlock()
-
-		go startMatch(client1, client2)
-	} else {
-		q.mu.Unlock()
-	}
-}
-
-func startMatch(c1, c2 *Client) {
-	time.Sleep(10 * time.Millisecond)
-
-	c1.SendMessage(Message{"type": "MatchFound"})
-	c2.SendMessage(Message{"type": "MatchFound"})
 }
 
 func (q *Queue) Leave(client *Client) {
